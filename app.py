23187976670f5ba736f6142ddaae0a4f2c2cea90
Khomsun Singhirunnusorn <ks0070@uah.edu>
2020-06-16 11:35:59 -0500

    2

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+import simplejson as json
+import requests
 
 app = Flask(__name__)
 
@@ -9,6 +11,24 @@ def index():
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+@app.route('/stock')
+def stock():
+	# api-endpoint 
+	URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=demo"
+	# location given here 
+	#location = "delhi technological university"
+  
+	# defining a params dict for the parameters to be sent to the API 
+    #PARAMS = {'address':location} 
+  
+    # sending get request and saving the response as response object 
+    r = requests.get(url = URL)#, params = PARAMS) 
+  
+    # extracting data in json format 
+    data = r.json()
+    print(data['Time Series (Daily)'][0]) 
+	return   
 
 if __name__ == '__main__':
   app.run(port=33507)
