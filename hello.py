f623af380d6354a05d2dd949e33aa28d336612b1
Khomsun Singhirunnusorn <ks0070@uah.edu>
2020-06-16 10:20:53 -0500

    3

--- hello.py
+++ hello.py
@@ -2,9 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def hello():
-	logging.debug("saying Hello")
     return 'Hello Heroku'
