17a210ad31feee1b6048e97fdc9819ca03c055ba
Khomsun Singhirunnusorn <ks0070@uah.edu>
2020-06-17 12:34:02 -0500

    1

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template, request, redirect
-from alpha_vantage.timeseries import TimeSeries
-from pprint import pprint
+from flask import Flask, render_template,redirect
+#from alpha_vantage.timeseries import TimeSeries
+#from pprint import pprint
 
 #import simplejson as json
-#import requests
+import requests
+#import urllib.request as urllib2
+import json
+import matplotlib.pyplot as plt
+import datetime
+import numpy as np
+
+'''
+req = urllib2.Request("http://api.open-notify.org/iss-now.json")
+response = urllib2.urlopen(req)
+
+obj = json.loads(response.read())
+
+print (obj['timestamp'])
+print (obj['iss_position']['latitude'], obj['iss_position']['longitude'])
+'''
 
 app = Flask(__name__)
 
@@ -17,21 +32,79 @@ def about():
 
 @app.route('/stock')
 def stock():
-  ts = TimeSeries(key='HGGFPH8DG45PWMAB', output_format='pandas')
-  data, meta_data = ts.get_intraday(symbol='MSFT',interval='1hr', outputsize='full')
+  '''
+  req = urllib2.Request("http://api.open-notify.org/iss-now.json")
+  response = urllib2.urlopen(req)
+  obj = json.loads(response.read())
+  print (obj['timestamp'])
+  print (obj['iss_position']['latitude'], obj['iss_position']['longitude'])
+  '''
+  '''
+  r = requests.get("http://api.open-notify.org/iss-now.json")
+  print(r.status_code)
+  data = r.json()
+  print(data['timestamp'])
+  print(data['iss_position']['latitude'], data['iss_position']['longitude'])
+  return render_template('plot.html', url='plot.png')
+  '''
+  
+  r = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=HGGFPH8DG45PWMAB")
+  data = r.json()
+  dic = data['Time Series (Daily)']
+  tod = datetime.datetime.now()
+  sdate=[]
+  sopen=[]
+  shigh=[]
+  slow=[]
+  sclose=[]
+  svol=[] 
+  for i in range(1,8):
+    d = datetime.timedelta(days = i)
+    a = tod - d
+    dt = str(a.date())
+    if dt in dic:
+      sdate.append(dt)
+      sopen.append(float(dic[dt]['1. open']))
+      shigh.append(float(dic[dt]['2. high']))
+      slow.append(float(dic[dt]['3. low']))
+      sclose.append(float(dic[dt]['4. close']))
+      svol.append(float(dic[dt]['5. volume']))
+  
+  plt.figure()#figsize=(9, 3))
+  plt.subplot(221)
+  plt.plot(sdate,sclose)
+  plt.subplot(222)
+  plt.plot(sdate,sclose)
+  plt.subplot(223)
+  plt.plot(sdate,sclose)
+  plt.subplot(224)
+  plt.plot(sdate,sclose)
+  plt.suptitle('Categorical Plotting')
+  plt.savefig('/template/stock.png')
+  print('pass')
+  return render_template('plot.html', url='/template/stock.png')
+  #plt.show()  
+
+  '''
+  ts = TimeSeries(key='HGGFPH8DG45PWMAB',retries='20',output_format='pandas')
+  data, meta_data = ts.get_intraday(symbol='IBM',interval='1min', outputsize='full')
   pprint(data.head(2))
-  # api-endpoint 
+  '''
+  # api-endpoint
+  #URL = "http://api.open-notify.org/iss-now.json"
   #URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=HGGFPH8DG45PWMAB"
+  #URL = "https://docs.google.com/spreadsheets/d/1dKmaV_JiWcG8XBoRgP8b4e9Eopkpgt7FL7nyspvzAsE"
   # location given here 
   #location = "delhi technological university"
   # defining a params dict for the parameters to be sent to the API 
   #PARAMS = {'address':location} 
   # sending get request and saving the response as response object 
-  #r = requests.get(url = URL)#, params = PARAMS) 
+  #r = requests.get("http://api.open-notify.org/iss-now.json")
+  #print(r.status_code)
   # extracting data in json format 
   #data = r.json()
   #print(data['Time Series (Daily)'][0])
-  return None  
-
+  
 if __name__ == '__main__':
-  app.run(port=33507)
+  #app.run(port=33507)
+  app.run()
