16ea13d0d9a10919752b5fcf6075200370ed1fbe
Khomsun Singhirunnusorn <ks0070@uah.edu>
2020-06-17 13:25:44 -0500

    1

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template,redirect
 import requests
 import matplotlib.pyplot as plt
 import datetime
-from bokeh.charts import Histogram
-from bokeh.embed import components
-import pandas as pd
+from bokeh.io import output_file, show
+from bokeh.plotting import figure
 
 app = Flask(__name__)
 
@@ -95,33 +94,17 @@ def stock():
   #data = r.json()
   #print(data['Time Series (Daily)'][0])
 
-iris_df = pd.read_csv("data/iris.data",names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"])
-feature_names = iris_df.columns[0:-1].values.tolist()
-
-# Create the main plot
-def create_figure(current_feature_name, bins):
-  p = Histogram(iris_df, current_feature_name, title=current_feature_name, color='Species', bins=bins, legend='top_right', width=600, height=400)
-
-  # Set the x axis label
-  p.xaxis.axis_label = current_feature_name
-
-  # Set the y axis label
-  p.yaxis.axis_label = 'Count'
-  return p
-
 @app.route('/iris')
 def iris():
-  # Determine the selected feature
-  current_feature_name = request.args.get("feature_name")
-  if current_feature_name == None:
-    current_feature_name = "Sepal Length"
-
-  # Create the plot
-  plot = create_figure(current_feature_name, 10)
-    
-  # Embed plot into HTML via Flask Render
-  script, div = components(plot)
-  return render_template("iris.html", script=script, div=div,feature_names=feature_names,  current_feature_name=current_feature_name)
+  output_file("template/bars.html")
+  fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+  counts = [5, 3, 4, 2, 4, 6]
+  plot = figure(x_range=fruits, plot_height=250, title="Fruit Counts",toolbar_location=None, tools="")
+  p.vbar(x=fruits, top=counts, width=0.9)
+  p.xgrid.grid_line_color = None
+  p.y_range.start = 0
+  show(p)
+  return render_template('bars.html')
   
 if __name__ == '__main__':
   #app.run(port=33507)
