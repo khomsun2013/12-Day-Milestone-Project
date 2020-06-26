554363d04a8dc0373237efcb19ea6719ae370a7e
Khomsun Singhirunnusorn <ks0070@uah.edu>
2020-06-25 23:58:06 -0500

    1

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template,redirect
+from flask import Flask,render_template,redirect,flash,url_for,request
 import requests
 import matplotlib.pyplot as plt
 import datetime
+import pandas as pd
 from bokeh.io import output_file, show, save
 from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.palettes import Category10
 
 app = Flask(__name__)
 
@@ -16,67 +18,128 @@ def index():
 def about():
   return render_template('about.html')
 
-@app.route('/stock')
-def stock():
-  '''
-  req = urllib2.Request("http://api.open-notify.org/iss-now.json")
-  response = urllib2.urlopen(req)
-  obj = json.loads(response.read())
-  print (obj['timestamp'])
-  print (obj['iss_position']['latitude'], obj['iss_position']['longitude'])
-  '''
-  '''
-  r = requests.get("http://api.open-notify.org/iss-now.json")
-  print(r.status_code)
-  data = r.json()
-  print(data['timestamp'])
-  print(data['iss_position']['latitude'], data['iss_position']['longitude'])
-  return render_template('plot.html', url='plot.png')
-  '''
-  
-  r = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=HGGFPH8DG45PWMAB")
-  data = r.json()
-  dic = data['Time Series (Daily)']
-  tod = datetime.datetime.now()
-  sdate=[]
-  sopen=[]
-  shigh=[]
-  slow=[]
-  sclose=[]
-  svol=[] 
-  for i in range(1,30):
-    d = datetime.timedelta(days = i)
-    a = tod - d
-    dt = str(a.date())
-    if dt in dic:
-      sdate.append(dt)
-      sopen.append(float(dic[dt]['1. open']))
-      shigh.append(float(dic[dt]['2. high']))
-      slow.append(float(dic[dt]['3. low']))
-      sclose.append(float(dic[dt]['4. close']))
-      svol.append(float(dic[dt]['5. volume']))
-  
-  '''
-  output_file("templates/stock.html")
-  source = ColumnDataSource(data=dict(
-    x=[i+1 for i in range(len(sdate)+1)],
-    y1=sopen,
-    y2=shigh,
-    y3=slow,
-    y4=sclose
-  ))
-  p = figure(plot_width=800, plot_height=600)
-  p.vline_stack(['y1', 'y2', 'y3', 'y4'], x='x', source=source)
-  show(p)
-  '''
-  output_file("templates/stock.html", mode='inline')
-  plot = figure(title='Stock IBM Daily', x_axis_label='Date',
-                  y_axis_label='Price')
-  plot.line(sdate, sopen, line_width=3, line_color='green', legend='Open')
-  plot.line(sdate, shigh, line_width=3, line_color='orange', legend='High')
-  show(plot)    
-  return render_template('stock.html')
+#def get_symbol(symbol):
+#    symbol_list = requests.get("http://chstocksearch.herokuapp.com/api/{}".format(symbol)).json()
+#    for x in symbol_list:
+#        if x['symbol'] == symbol:
+#            return x['company']
 
+@app.route('/stock',methods=['GET','POST'])
+def stock():
+  try:
+    if (request.method == 'POST'):
+      dst = request.form.to_dict()
+      sel=[0]*4
+      if('Open'in dst): sel[0]=1
+      if('High'in dst): sel[1]=1 
+      if('Low'in dst): sel[2]=1
+      if('Close'in dst): sel[3]=1
+      if sum(sel)==0:
+        return render_template('index.html')
+      r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+dst['stocks']+'&outputsize=full&apikey=HGGFPH8DG45PWMAB')
+      data = r.json()
+      print(r.status_code)
+      dic = data['Time Series (Daily)']
+      pdic=pd.DataFrame(dic)
+      pdic=pdic.T
+      x=range(1,31)
+      pd30 = pdic.head(30)
+      output_file('templates/'+dst['stocks']+'.html',mode='inline')
+      p2 = figure(title='Stock Prices '+dst['stocks']+' Back in 30 Days', x_axis_label='Date',y_axis_label='Price')
+      grpo=['Open']*30
+      grph=['High']*30
+      grpl=['Low']*30
+      grpc=['Close']*30
+      grp_list=['Open','High','Low','Close']
+      colors=['green','orange','blue','red']
+      for i in range(4):
+        if(i==0)and(sel[0]==1):
+          source = ColumnDataSource(
+          data={'x':x,
+                'date':list(pd30.index.values),
+                'group':grpo,
+                'y':list(pd30['1. open'].values)})
+        elif(i==1)and(sel[1]==1):
+          source = ColumnDataSource(
+          data={'x':x,
+                'date':list(pd30.index.values),
+                'group':grph,
+                'y':list(pd30['2. high'].values)})
+        elif(i==2)and(sel[2]==1):
+          source = ColumnDataSource(
+          data={'x':x,
+                'date':list(pd30.index.values),
+                'group':grpl,
+                'y':list(pd30['3. low'].values)})
+        elif(i==3)and(sel[3]==1):
+          source = ColumnDataSource(
+          data={'x':x,
+                'date':list(pd30.index.values),
+                'group':grpc,
+                'y':list(pd30['4. close'].values)})
+        if sel[i]==1:  
+          p2.line(x='x',y='y',source=source,legend_label = grp_list[i],color = colors[i])
+          p2.circle(x='x', y='y', fill_color=colors[i],line_color=colors[i], size=8,source=source,legend_label = grp_list[i])
+      #add tool tips
+      hover = HoverTool(tooltips =[('Type: ','@group'),('Date: ','@date'),('Price: ','@y')])
+      p2.add_tools(hover)
+      save(p2)
+
+      '''
+      tod = datetime.datetime.now()
+      sdate=[]
+      sopen=[]
+      shigh=[]
+      slow=[]
+      sclose=[]
+      svol=[] 
+      for i in range(1,30):
+        d = datetime.timedelta(days = i)
+        a = tod - d
+        dt = str(a.date())
+        if dt in dic:
+          sdate.append(dt)
+          sopen.append(float(dic[dt]['1. open']))
+          shigh.append(float(dic[dt]['2. high']))
+          slow.append(float(dic[dt]['3. low']))
+          sclose.append(float(dic[dt]['4. close']))
+          svol.append(float(dic[dt]['5. volume']))
+      x=[i+1 for i in range(len(sdate))]
+      source_open = ColumnDataSource(data=dict(
+          date=x,
+          open=sopen,
+          close=sclose,
+          sdate=sdate,
+      ))
+      output_file('templates/'+dst['stocks']+'.html',mode='inline')
+      #plot = figure(title='Stock IBM Daily', x_axis_label='Date',
+      #                y_axis_label='Price',x_range=sdate)
+      #plot.circle(sdate, sopen, fill_color="green",line_color='green', size=8)
+      plot = figure(title='Stock '+dst['stocks']+' Back in 30 Days', x_axis_label='Date',y_axis_label='Price')
+      plot.line('date', 'open', line_width=2, line_color='green', legend_label='Open', source=source_open)
+      plot.circle('date', 'open', fill_color="green",line_color='green', size=8,source=source_open)
+      plot.line('date', 'close', line_width=2, line_color='red', legend_label='Close', source=source_open)
+      plot.circle('date', 'close', fill_color="red",line_color='red', size=8,source=source_open)
+      #plot.line('date', sopen, line_width=3, line_color='green', legend_label='Open')
+      #plot.line(x, shigh, line_width=3, line_color='orange', legend_label='High')
+      #plot.line(x, slow, line_width=3, line_color='red', legend_label='Low')
+      #plot.line(x, sclose, line_width=3, line_color='blue', legend_label='Close')
+      
+      #hover = HoverTool()
+      #hover.tooltips = """
+      #<div style=padding=5px>DATE:@sdate</div>
+      #<div style=padding=5px>OPEN:@open</div>
+      #"""
+      #plot.add_tools(hover)
+      #show(plot) 
+      plot.add_tools(HoverTool(show_arrow=False,tooltips=[("DATE", "@sdate"), ("OPEN", "@open"), ("CLOSE", "@close")]))
+      save(plot)
+      '''
+
+      return render_template(dst['stocks']+'.html')
+  except Exception as e:
+      flash(e)
+      return render_template('index.html')
   '''
   plt.figure()#figsize=(9, 3))
   plt.subplot(221)
@@ -91,8 +154,6 @@ def stock():
   plt.savefig('/template/stock.png')
   return render_template('plot.html', url='/template/stock.png')
   '''
-  #plt.show()  
-
   '''
   ts = TimeSeries(key='HGGFPH8DG45PWMAB',retries='20',output_format='pandas')
   data, meta_data = ts.get_intraday(symbol='IBM',interval='1min', outputsize='full')
@@ -112,19 +173,24 @@ def stock():
   # extracting data in json format 
   #data = r.json()
   #print(data['Time Series (Daily)'][0])
+  '''
+  req = urllib2.Request("http://api.open-notify.org/iss-now.json")
+  response = urllib2.urlopen(req)
+  obj = json.loads(response.read())
+  print (obj['timestamp'])
+  print (obj['iss_position']['latitude'], obj['iss_position']['longitude'])
+  '''
+  '''
+  r = requests.get("http://api.open-notify.org/iss-now.json")
+  print(r.status_code)
+  data = r.json()
+  print(data['timestamp'])
+  print(data['iss_position']['latitude'], data['iss_position']['longitude'])
+  return render_template('plot.html', url='plot.png')
+  '''
 
-@app.route('/iris')
-def iris():
-  output_file("templates/bars.html")
-  fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
-  counts = [5, 3, 4, 2, 4, 6]
-  p = figure(x_range=fruits, plot_height=250, title="Fruit Counts",toolbar_location=None, tools="")
-  p.vbar(x=fruits, top=counts, width=0.9)
-  p.xgrid.grid_line_color = None
-  p.y_range.start = 0
-  show(p)
-  return render_template('bars.html')
-  
 if __name__ == '__main__':
-  #app.run(port=33507)
-  app.run(debug='True')
+  app.run(port=33507)
+  #app.run(debug='True')
+'''
+'''      
